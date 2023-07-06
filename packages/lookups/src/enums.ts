c72e69c8eb6925ff9bfc786c9b87deb34ec9dc49
amandesai01 <amandesai01@gmail.com>
2023-07-06 14:24:03 +0530

    pkg:lookups init

---
+++ packages/lookups/src/enums.ts
@@ -0,0 +1,21 @@
+export enum Currency {
+  INR,
+  USD,
+}
+
+export enum RecordType {
+  Expense,
+  Income,
+  Transfer,
+}
+
+export enum RecordDirection {
+  Right,
+  Left,
+}
+
+export enum EndRecurrenceBy {
+  Count,
+  EndDate,
+  NeverEnd,
+}
