4ca3cd04aa869e6b5f1cd73e8762b4cfb21b4f26
amandesai01 <amandesai01@gmail.com>
2023-07-20 18:36:33 +0530

    web: re structure sidebar

--- apps/web/src/components/sidebar/GeneralSidebar.tsx
+++ apps/web/src/components/sidebar/GeneralSidebar.tsx
@@ -8,15 +8,11 @@ function GeneralSidebar() {
       <div>
         <ul className="flex flex-nowrap md:block mr-3 md:mr-0">
           <SidebarOption pathName="/home" icon="HomeIcon" label="Home" />
-          <SidebarOption pathName="/analytics" icon="AnalyticsIcon" label="Analytics" />
           <SidebarOption pathName="/budgets" icon="BudgetIcon" label="Budgets" />
           <SidebarOption pathName="/transactions" icon="TransactionIcon" label="Transactions" />
           <SidebarOption pathName="/accounts" icon="AccountIcon" label="Accounts" />
-          <br />
-          <span className="text-xs font-bold pl-2">ASSETS</span>
           <SidebarOption pathName="/labels" icon="LabelsIcon" label="Labels" />
           <SidebarOption pathName="/categories" icon="CategoriesIcon" label="Categories" />
-          <SidebarOption pathName="/triggers" icon="TriggersIcon" label="Triggers" />
           <SidebarOption pathName="/recurring-transactions" icon="RecurringTransactionIcon" label="Recurring Transactions" />
           <hr className="my-4" />
           <SidebarOption pathName="/settings" icon="SettingsIcon" label="Settings" />
